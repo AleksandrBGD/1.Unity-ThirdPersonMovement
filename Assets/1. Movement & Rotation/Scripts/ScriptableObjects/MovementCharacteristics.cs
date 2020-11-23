fd030345f82f9d1c44f6c06944c127778147171d
Piton <BotUnityGameDev@gmail.com>
2020-11-23 18:29:16 +0300

    1. Movement & Rotate
    Third Person Movement - Movement And Rotate.

---
+++ Assets/1. Movement & Rotation/Scripts/ScriptableObjects/MovementCharacteristics.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BGD.ThirdPersonMovement_1
+{
+
+    [CreateAssetMenu(fileName = "Characteristics", menuName = "Movement/MovementCharacteristics", order = 1)]
+    public class MovementCharacteristics : ScriptableObject
+    {
+        [SerializeField] private bool _visibleCursor = false;
+        
+        [SerializeField] private float _movementSpeed = 1f;
+        [SerializeField] private float _angularSpeed = 150f;
+
+        [SerializeField] private float _gravity = 15f;
+
+
+        public bool VisibleCursor => _visibleCursor;
+
+        public float MovementSpeed => _movementSpeed;
+        public float AngularSpeed => _angularSpeed;
+
+        public float Gravity => _gravity;
+    }
+}
