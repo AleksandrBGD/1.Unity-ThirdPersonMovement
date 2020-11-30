b96ee7ddd7bf3cd95587bde8e2d7cb5e01184639
Piton <BotUnityGameDev@gmail.com>
2020-11-30 20:45:18 +0300

    3. Run & Jump
    Third Person Movement - Jump & Run

---
+++ Assets/3. Run & Jump/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+namespace BGD.ThirdPersonMovement_1
+{
+
+    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(Animator))]
+    public class ThirdPersonMovement : MonoBehaviour
+    {
+        [SerializeField] private Transform _camera;
+        [SerializeField] private MovementCharacteristics _characteristics;
+
+        private float _vertical, _horizontal, _run;
+
+        private readonly string STR_VERTICAL = "Vertical";
+        private readonly string STR_HORIZONTAL = "Horizontal";
+        private readonly string STR_RUN = "Run";
+        private readonly string STR_JUMP = "Jump";
+
+        private const float DISTANCE_OFFSET_CAMERA = 5f;
+
+        private CharacterController _controller;
+        private Animator _animator;
+
+        private Vector3 _direction;
+        private Quaternion _look;
+
+        private Vector3 TargetRotate => _camera.forward * DISTANCE_OFFSET_CAMERA;
+        private bool Idle => _horizontal == 0.0f && _vertical == 0.0f;
+
+
+        private void Start()
+        {
+            _controller = GetComponent<CharacterController>();
+            _animator = GetComponent<Animator>();
+
+            Cursor.visible = _characteristics.VisibleCursor;
+        }
+
+        private void Update()
+        {
+            Movement();
+            Rotate();
+        }
+
+        private void Movement()
+        {
+            if (_controller.isGrounded)
+            {
+                _horizontal = Input.GetAxis(STR_HORIZONTAL);
+                _vertical = Input.GetAxis(STR_VERTICAL);
+                _run = Input.GetAxis(STR_RUN);
+
+                _direction = transform.TransformDirection(_horizontal, 0, _vertical).normalized;
+
+                PlayAnimation();
+                Jump();
+            }
+
+            _direction.y -= _characteristics.Gravity * Time.deltaTime;
+
+            float speed = _run * _characteristics.RunSpeed + _characteristics.MovementSpeed; 
+            Vector3 dir = _direction * speed * Time.deltaTime;
+
+            dir.y = _direction.y;
+
+            _controller.Move(dir);
+        }
+
+        private void Rotate()
+        {
+            if (Idle) return;
+
+            Vector3 target = TargetRotate;
+            target.y = 0;
+
+            _look = Quaternion.LookRotation(target);
+
+            float speed = _characteristics.AngularSpeed * Time.deltaTime;
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, _look, speed);
+        }
+
+        private void Jump()
+        {
+            if(Input.GetButtonDown(STR_JUMP))
+            {
+                _animator.SetTrigger(STR_JUMP);
+                _direction.y += _characteristics.JumpForce;
+            }
+        }
+
+        private void PlayAnimation()
+        {
+            float horizontal = _run * _horizontal + _horizontal;
+            float vertical = _run * _vertical + _vertical;
+
+            _animator.SetFloat(STR_VERTICAL, vertical);
+            _animator.SetFloat(STR_HORIZONTAL, horizontal);
+        }
+    }
+}
