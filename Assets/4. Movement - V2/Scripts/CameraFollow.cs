027008b0f6a8135d28c38ea1268e3c31bffadb0c
Piton <BotUnityGameDev@gmail.com>
2020-12-02 15:45:38 +0300

    4. Movement - V2
    Third Person Movement V2.0

---
+++ Assets/4. Movement - V2/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace BGD.ThirdPersonMovement_2
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] [Range(1f, 5f)] private float _angularSpeed = 1f;
+
+        [SerializeField] private Transform _target;
+
+        private float _angleY;
+
+        private void Start()
+        {
+            _angleY = transform.rotation.y;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Z)) _angleY -= _angularSpeed;
+            if (Input.GetKey(KeyCode.X)) _angleY += _angularSpeed;
+
+            transform.position = _target.transform.position;
+            transform.rotation = Quaternion.Euler(0, _angleY, 0);
+        }
+    }
+}
---
+++ Assets/4. Movement - V2/Scripts/ScriptableObjects/MovementCharacteristics.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace BGD.ThirdPersonMovement_2
+{
+
+    [CreateAssetMenu(fileName = "Characteristics", menuName = "Movement/MovementCharacteristics", order = 1)]
+    public class MovementCharacteristics : ScriptableObject
+    {
+        [SerializeField] private bool _visibleCursor = false;
+        
+        [SerializeField] private float _movementSpeed = 1f;
+        [SerializeField] private float _runSpeed = 1.5f;
+
+        [SerializeField] private float _angularSpeed = 150f;
+
+        [SerializeField] private float _gravity = 15f;
+        [SerializeField] private float _jumpForce = 7f;
+
+
+        public bool VisibleCursor => _visibleCursor;
+
+        public float MovementSpeed => _movementSpeed;
+        public float RunSpeed => _runSpeed;
+
+        public float AngularSpeed => _angularSpeed;
+
+        public float Gravity => _gravity / 10f;
+        public float JumpForce => _jumpForce / 100f;
+
+    }
+}
